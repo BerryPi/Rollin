c93addf4f8e9fd5048e6c0f6e3be92a8527401dc
BerryPi <naturalneopoint@gmail.com>
2016-02-04 00:11:21 -0500

    Made assets and source code available

---
+++ Assets/Scripts/PlayAreaController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayAreaController : MonoBehaviour {
+	
+	public float speed;
+	public GameObject pickup;
+	public Text gameEndText;
+
+	private float endTime; 
+
+	private Vector3 rot = new Vector3 (0.0f, 0.0f, 90.0f); 
+    
+	void Start()
+	{
+		endTime = Time.time + 60.0f;
+		UpdateTime ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rot.x += Input.GetAxis ("Horizontal"); 
+		rb.rotation = Quaternion.Euler(rot);
+	}
+
+	void Update()
+	{
+		if (GameObject.FindWithTag ("Pickup") == null && Time.time < endTime) {
+			Vector3 spawnPos = Random.insideUnitSphere * 0.8f;
+			spawnPos.x = 0.0f;
+			Instantiate(pickup, spawnPos, Quaternion.identity);
+		}
+		if (Time.time >= endTime) {
+			Destroy(GameObject.FindWithTag("Pickup"));
+		}
+		if (Input.GetButton ("Jump") && Time.time >= endTime)
+			NewGame ();
+		UpdateTime ();
+	}
+
+	void UpdateTime()
+	{
+		if (Time.time < endTime) {
+			gameEndText.text = "Time: " + (endTime - Time.time).ToString ("F1") + "s";
+		} 
+		else {
+			gameEndText.text = "Game Over!";
+		}
+	}
+
+	void NewGame()
+	{
+		GameObject.FindWithTag ("Player").transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
+		GameObject.FindWithTag ("Player").GetComponent<PlayerController> ().ResetScore ();
+		rot = new Vector3 (0.0f, 0.0f, 90.0f);
+		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (rot);
+		endTime = Time.time + 60.0f;
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float jumpSpeed;
+	public Text scoreText;
+	private int score = 0;
+
+	void Start()
+	{
+		UpdateScore ();
+	}
+
+	void FixedUpdate () {
+	if (Input.GetAxis ("Vertical") > 0)
+			GetComponent<Rigidbody> ().velocity = (Vector3.up * jumpSpeed);
+
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Pickup") {
+			Destroy (other.gameObject);
+			score += 1;
+			UpdateScore ();
+		}
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + (score/2).ToString ();
+	}
+
+	public void ResetScore()
+	{
+		score = 0;
+		UpdateScore ();
+	}
+}
